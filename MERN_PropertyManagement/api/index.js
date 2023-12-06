ba1b454ac73c72ca46bcfcfcb5144127da8ae39e
Nikhil Yadav <nickelyadav57@gmail.com>
2023-12-06 17:33:12 +0530

    :card_file_box: chore: Connects to mongo db

--- MERN_PropertyManagement/api/index.js
+++ MERN_PropertyManagement/api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((e) => {
+    console.log(e)
+})
 
 const app = express();
 
