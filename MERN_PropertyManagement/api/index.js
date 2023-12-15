47866133d4653f59cc52f245cd01e8fc31aa28de
Nikhil Yadav <nickelyadav57@gmail.com>
2023-12-15 10:37:23 +0530

    :building_construction: feat: Completes basic MVC type architecture setup for API with passing tests

--- MERN_PropertyManagement/api/index.js
+++ MERN_PropertyManagement/api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+import router from "./routes/user.route.js";
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_DB).then(() => {
@@ -14,3 +15,5 @@ const app = express();
 app.listen(3000, () => {
     console.log("service running on port 3000")
 })
+
+app.use("/api/user", router)
