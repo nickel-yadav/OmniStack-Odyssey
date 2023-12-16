7a586458db3dae4515159c49590fdd64e3cebf19
Nikhil Yadav <nickelyadav57@gmail.com>
2023-12-16 08:18:01 +0530

    :goal_net: feat: Implements error handling middleware for graceful error handling

--- MERN_PropertyManagement/api/index.js
+++ MERN_PropertyManagement/api/index.js
@@ -21,3 +21,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", UserRouter)
 app.use("/api/auth", AuthRouter)
+
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500; 
+    const message = error.message || 'Internal server error'; 
+    return res.status(statusCode).json({
+        success: false, 
+        statusCode, 
+        message,  
+    })
+})
