e9c461dc67e0b971f958270beaefba4d71a92866
Nikhil Yadav <nickelyadav57@gmail.com>
2023-12-20 17:34:23 +0530

    :goal_net: feat: Adds graceful error handling and loading states

--- MERN_PropertyManagement/client/src/pages/SignUp.tsx
+++ MERN_PropertyManagement/client/src/pages/SignUp.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
   const handleChange = (e: any) => {
+    // FIXME: Update type
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
@@ -11,48 +16,70 @@ export default function SignUp() {
   };
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const res = await fetch("/api/auth/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log("daadshf", data);
+    // FIXME: Update type
+    try {
+      e.preventDefault();
+      setIsLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setIsLoading(false);
+        return;
+      }
+      setIsLoading(false);
+      navigate("/sign-in");
+    } catch (error: any) {
+      // FIXME: Update type
+      setIsLoading(false);
+      setError(error.message);
+    }
   };
 
   return (
     <div className="flex-grow flex justify-center items-start bg-gradient-to-b from-gray-400 to-gray-900">
-      <div className="mt-10 md:mt-40 lg:mt-10 w-4/5 lg:w-1/3 p-4 bg-clip-padding backdrop-filter backdrop-blur-3xl border border-gray-100 shadow-lg rounded-lg flex flex-col justify-between bg-gray-600 bg-opacity-40">
+      <div className="mt-10 md:mt-40 lg:mt-10 w-4/5 lg:w-1/3 p-4 bg-clip-padding backdrop-filter backdrop-blur-3xl border border-gray-100 shadow-lg rounded-lg flex flex-col justify-between bg-white bg-opacity-20">
         <h2 className="text-center text-white text-2xl py-4">Sign In</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4 md:gap-6">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 md:gap-3">
           <input
             type="text"
             placeholder="Name"
             id="username"
-            className="p-2 rounded-2xl border"
+            className="p-2 rounded-lg border-2 border-gray-500"
             onChange={handleChange}
           />
           <input
-            type="text"
+            type="email"
             placeholder="Email Address"
             id="email"
-            className="p-2 rounded-2xl border"
+            className="p-2 rounded-lg border-2 border-gray-500"
             onChange={handleChange}
           />
           <input
-            type="text"
+            type="password"
             placeholder="Password"
             id="password"
-            className="p-2 rounded-2xl border"
+            className="p-2 rounded-lg border-2 border-gray-500"
             onChange={handleChange}
           />
-          <button className="border p-2 rounded-2xl hover:bg-blue-600 bg-blue-500 text-white">
-            Sign Up
+          <button
+            disabled={isLoading}
+            className="border p-2 rounded-lg disabled:bg-gray-500 hover:bg-blue-600 bg-blue-500 text-white"
+          >
+            {isLoading ? "Loading..." : "Sign Up"}
           </button>
         </form>
+        {error && (
+          <div className="p-4 mt-2 bg-red-200 border-s-4 border-red-600">
+            <p className="text-red-500 text-center">{error}</p>
+          </div>
+        )}
         <div className="flex flex-col items-center pt-4">
           <p className="text-white">Have an account ?</p>
           <p className="text-white hover:text-blue-500">
