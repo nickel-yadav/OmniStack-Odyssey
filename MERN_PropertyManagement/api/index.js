27c285a9180073ee6f471e368efa7a018030e022
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-06 12:48:10 +0530

    :truck: chore: Moves repo for better organisation

--- MERN_PropertyManagement/api/index.js
+++ MERN_PropertyManagement/api/index.js
@@ -1,33 +1,36 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import UserRouter from "./routes/user.route.js";
-import AuthRouter from './routes/auth.route.js';
-dotenv.config()
+import AuthRouter from "./routes/auth.route.js";
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB).then(() => {
-    console.log("Connected to MongoDB")
-}).catch((e) => {
-    console.log(e)
-})
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("service running on port 3000")
-})
+  console.log("service running on port 3000");
+});
 
-app.use("/api/user", UserRouter)
-app.use("/api/auth", AuthRouter)
+app.use("/api/user", UserRouter);
+app.use("/api/auth", AuthRouter);
 
 app.use((error, req, res, next) => {
-    const statusCode = error.statusCode || 500; 
-    const message = error.message || 'Internal server error'; 
-    return res.status(statusCode).json({
-        success: false, 
-        statusCode, 
-        message,  
-    })
-})
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
