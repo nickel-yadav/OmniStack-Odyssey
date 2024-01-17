6877ba98833d811a2da7679fa392c3bea7e460e0
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-17 18:16:54 +0530

    :tada: init: Intializes spring boot backend app

---
+++ SJMR_MoviewReview/moviereview/src/main/java/dev/nikhilyadav/moviereview/MoviereviewApplication.java
@@ -0,0 +1,15 @@
+package dev.nikhilyadav.moviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviereviewApplication.class, args);
+	}
+
+}
