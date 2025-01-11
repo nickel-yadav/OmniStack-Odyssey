ca864a327995d93b2ab41f15bd855afe1a280ff4
Nickel Yadav <nickelyadav57@gmail.com>
2025-01-11 21:58:49 +0530

    :sparkles: feat: Creates tested controller for home

---
+++ TacoCloud/src/main/java/com/tacoCloud/TacoCloud/HomeController.java
@@ -0,0 +1,12 @@
+package com.tacoCloud.TacoCloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
